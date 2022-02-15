2b0775a7879a692fbc41978280e66b12a40f2951
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2022-02-15 18:06:21 +0100

    Initial commit

--- obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace slider {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/slider;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/slider;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
